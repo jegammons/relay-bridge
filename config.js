40c123684680f8038464a99fe935ba9443b9aa69
jegammons <jgmarines@aol.com>
2026-08-15 13:35:26 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.CIPHER_BRIDGE_CONFIG = {
+    containerIdentifier: "iCloud.com.jakobgammons.Cipher",
+    apiToken: "02f1d2f080971f6ded795070383dcfeaf93bc9af3fc2decf00deb4993a31148e",
+    environment: "production"
+  };
